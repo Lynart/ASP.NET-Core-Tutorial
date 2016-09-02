2c281b45f5e736af99aed95f4a7b212f0c293c29
Vince Lynart <Vince Lynart>
2016-09-02 16:48:59 -0400

    Bootstrap next!

---
+++ src/NETCoreWebExample/Controllers/Web/AppController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using NETCoreWebExample.Services;
+using NETCoreWebExample.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETCoreWebExample.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com"))
+            {
+                //Empty first option will cause the whole object to be false
+                ModelState.AddModelError("Email", "We don't support AOL addresses");
+            }
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From Me", model.Message);
+                //Clears model
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message sent";
+            }
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
--- src/NETCoreWebExample/Startup.cs
+++ src/NETCoreWebExample/Startup.cs
@@ -5,29 +5,80 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using NETCoreWebExample.Services;
 
 namespace NETCoreWebExample
 {
     public class Startup
     {
+        private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+
+            //Stores the json config file
+            //Note .SetBasePath must be called or error is thrown
+            //Environment variables will overwrite Json file
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_env.ContentRootPath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            _config = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Singleton because the config file should only exist once
+            services.AddSingleton(_config);
+            //ATTENTION - MVC requires classes, interfaces, etc.
+            //This will register it
+            services.AddMvc();
+
+            if (_env.IsDevelopment())
+            {
+                //This is needed for running services on controllers
+                services.AddScoped<IMailService, DebugMailService>();
+            }
+            else
+            {
+                //Real service here
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            //Used to see errors, would only apply to development machines
+            //You can set what the machine is in the project properties/debug
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
             //ATTENTION -  Reverse order will fail. Default files sets project to look for default files
             //Use static files tells web app to serve static files. Calling in reverse results in no 
             //default files found by UseStaticFiles
-            app.UseDefaultFiles();
+            //app.UseDefaultFiles();
             //Need to add a package. Check project.json for
             //"Microsoft.AspNetCore.StaticFiles": "1.0.0"
             app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                //Can be written like a regular method, this makes it more readable
+                config.MapRoute(
+                    name: "Default",
+                    //Note ID is optional
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index" }
+                    );
+            });
         }
     }
 }
